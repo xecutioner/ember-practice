3d4dc426dd7bb30bb7bc5cbe5fad1b07b7d5a3ba
xecutioner <kapil@jyaasa.com>
2016-04-05 18:49:05 +0545

    added missing semicolon

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return [ 'Architect 1 ', 'Architect 2', 'Architect 3' ]
+    return [ 'Architect 1 ', 'Architect 2', 'Architect 3' ];
   }
 });
